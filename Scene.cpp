ad8a6f481ab50ad47114ef58838f64f5286196f8
Jaime Arroyo <jaime.arroyom96@gmail.com>
2018-03-27 19:31:50 +0200

    Camera height *0.5f

--- Scene.cpp
+++ Scene.cpp
@@ -79,7 +79,7 @@ void Scene::initFinishDoor() {
 void Scene::init()
 {
 	
-	glm::vec2 geom[2] = { glm::vec2(0.f, 0.f), glm::vec2(float(CAMERA_WIDTH), float(CAMERA_HEIGHT)) };
+	glm::vec2 geom[2] = { glm::vec2(0.f, 0.f), glm::vec2(float(CAMERA_WIDTH), float(CAMERA_HEIGHT)*0.5f) };
 
 	glm::vec2 texCoords[2] = { glm::vec2(120.f / 512.0, 0.f), glm::vec2((120.f + 320.f) / 512.0f, 160.f / 256.0f) };
 	projection = glm::ortho(0.f, float(CAMERA_WIDTH - 1), float(CAMERA_HEIGHT - 1), 0.f);
